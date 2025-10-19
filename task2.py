3b95adf711ea0074084bba278a3d995593d99744
NasraDecoded <nashomar03@gmail.com>
2025-10-19 20:29:59 +0200

    Add task2.py

---
+++ task2.py
@@ -0,0 +1,21 @@
+#Hello Nasra Welcome to beetroot 
+print ("Hello Nasra,Welcome to beetroot")
+
+#Multiple things one print 
+#mom dad sister brother
+print('mom', 'dad',  'sister', 'brother')
+
+#mom-dad-sister-brother,sep- means use dash to separate them
+print('mom', 'dad', 'sister', 'brother,',sep='-' )
+
+#Mom Dad Sister Brother
+#No new line at the end,the next word will continue in the same line 
+print('Mom', 'Dad', 'Sister', 'Brother', end='')
+
+#Output Mom Dad Sister Brother done!
+#End the line with done! and a new line should be created. 
+print('Mom', 'Dad', 'Sister', 'Brother', end=' done!\n')
+
+#Hello|Princess|welcome|to|wonderland
+#Telling python lines to be separated by |
+print('Hello', 'Princess', 'welcome', 'to' ,'wonderland', sep='|')
