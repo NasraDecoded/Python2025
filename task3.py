fab993b31178a8b77949590709831baee4286503
Nasra Omar <nashomar03@gmail.com>
2025-10-19 21:01:03 +0200

    Update task2.py

---
+++ task3.py
@@ -0,0 +1,7 @@
+# Print letter O using \n and \t
+#\n creates a new line 
+print("#########\n#\t#\n#\t#\n#\t#\n#########")
+
+#\t creates horizontal tab
+# Print letter H using \n and \t
+print("\n#\t#\n#\t#\n#########\n#\t#\n#\t#")
